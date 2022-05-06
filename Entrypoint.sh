4dbb6e725dee864add8be2ac5ba5721632ff7f0f
Ryan <ryanvanmassenhoven@gmail.com>
2022-05-06 16:00:44 -0400

    Add Logic to load existing Config if one exists

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+if [ "$(ls -A /Config)" ]
+then
+        rm -rf /usr/lib/unifi
+        ln -s /Config /usr/lib/unifi
+else
+        cp -r /usr/lib/unifi/* /Config
+        rm -rf /usr/lib/unifi
+        ln -s /Config /usr/lib/unifi
+fi
+
 java -jar /usr/lib/unifi/lib/ace.jar start
